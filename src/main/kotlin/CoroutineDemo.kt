8286d79b8a3184559a65be9e2244fe8f951df48f
Martin L. Jensen <martinloesethjensen@gmail.com>
2019-09-09 17:07:46 +0200

    Running a coroutine with launch

--- src/main/kotlin/CoroutineDemo.kt
+++ src/main/kotlin/CoroutineDemo.kt
@@ -1,5 +1,29 @@
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.lang.Thread.sleep
+import kotlin.concurrent.thread
+import kotlin.time.ExperimentalTime
+import kotlin.time.seconds
 
-fun main() = runBlocking {
-    println("Hello")
+@ExperimentalTime
+fun main() {
+
+    GlobalScope.launch {
+        delay(1.seconds.toLongMilliseconds()) // does not block the thread
+        print("world")
+    }
+
+    print("Hello, ")
+    sleep(1500)
+
+//    Not coroutines
+//    thread {
+//        sleep(1000)
+//        print("world")
+//    }
+//
+//    print("Hello, ")
+//    sleep(1500)
+
 }
